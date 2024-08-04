7cd523fdc4665298cc245b2714553934f91ddb95
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2024-08-04 17:21:54 +0530

    dx

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,43 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import LandingPage from "./pages/LandingPage";
-import AddClient from "./pages/AddClient";
 import Navbar from "./components/Navbar";
 import LandingPageSection9 from "./components/LandingPageSection9";
 import { AuthAdvocateContext } from "./context/AuthAdvocateContext";
 import Private from "./components/Private";
-
+import AddClient from "./pages/AddClient";
 const App = () => {
   const [isAdvocateLoggedIn, setIsAdvocateLoggedIn] = useState(
     JSON.parse(localStorage.getItem("acms_advocate_user"))
   );
-
   return (
-    <AuthAdvocateContext.Provider
-      value={{ isAdvocateLoggedIn, setIsAdvocateLoggedIn }}
-    >
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route
-            path="/add-client"
-            element={<Private Component={AddClient} />}
-          />
-        </Routes>
-        <LandingPageSection9 />
-      </Router>
-    </AuthAdvocateContext.Provider>
+    <>
+      <AuthAdvocateContext.Provider
+        value={{ isAdvocateLoggedIn, setIsAdvocateLoggedIn }}
+      >
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/add-client"
+              element={<Private Component={AddClient} />}
+            />
+          </Routes>
+          <LandingPageSection9 />
+        </BrowserRouter>
+      </AuthAdvocateContext.Provider>
+    </>
   );
 };
 
